b7bf3420d98753b8ef01a199bbe3cf76a96d07fe
Thijs Schreijer <thijs@thijsschreijer.nl>
2013-05-06 20:21:45 +0200

    removed some debug stuff

--- src/upnpgateway.lua
+++ src/upnpgateway.lua
@@ -275,24 +275,6 @@ if args then
     os.exit(exitcodes.ERROR)
   end
   
-  --------------------------------
-  -- TEMP STUFF
-  --------------------------------
-  local copas = require("copas.timer")
-  local dss = require("dss")
-  local tf = function()
-    if copas.isexiting() == false then
-      -- running and not scheduled to stop
-      print("DSS has ",dss.queuesize()," elements")
-    end
-  end
-  
-  local tmr = copas.newtimer(nil,tf, nil, true, nil):arm(5)
-  print("armed timer", tmr)
-  --------------------------------
-  -- TEMP STUFF
-  --------------------------------
-
   -- start the engine by starting the copas loop
   logger:info("+------------------------------------+")
   logger:info("|      Starting the main loop        |")
